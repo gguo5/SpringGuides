6722812d0403d4ae392b2d2065abab304ac68476
Grant <grant_guo@dataagility.com>
2015-05-25 16:29:13 +1000

    init commit

---
+++ src/main/java/com/gguo/springguides/consumingrest/Application.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gguo.springguides.consumingrest;
+
+
+import org.springframework.web.client.RestTemplate;
+
+public class Application {
+
+    public static void main(String args[]) {
+        RestTemplate restTemplate = new RestTemplate();
+        Page page = restTemplate.getForObject("http://graph.facebook.com/pivotalsoftware", Page.class);
+        System.out.println("Name:    " + page.getName());
+        System.out.println("About:   " + page.getAbout());
+        System.out.println("Phone:   " + page.getPhone());
+        System.out.println("Website: " + page.getWebsite());
+        System.out.println("ID: " + page.getId());
+    }
+
+}
---
+++ src/main/java/com/gguo/springguides/consumingrest/Page.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gguo.springguides.consumingrest;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Page {
+
+    private String name;
+    private String about;
+    private String phone;
+    private String website;
+    private String id;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public String getId() {
+        return id;
+    }
+    
+   
+
+}
